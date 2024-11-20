3875d8b6418a4860de542f48bf326e98ff7eacd7
martinstorvoll <martin.storvoll@nav.no>
2024-11-20 11:22:32 +0100

    fix

--- src/app/api/token/route.ts
+++ src/app/api/token/route.ts
@@ -1,14 +1,30 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { generateBearerToken } from '../simuler/route'
+import { requestAzureClientCredentialsToken } from '@navikt/oasis'
+
+const generateBearerToken = async (): Promise<string> => {
+  const clientCredentials = await requestAzureClientCredentialsToken(
+    'api://dev-gcp.pensjonskalkulator.pensjonskalkulator-backend/.default'
+  )
+
+  if (clientCredentials.ok) {
+    return clientCredentials.token
+  } else {
+    console.error('Failed to generate token: ', clientCredentials)
+    throw new Error('Failed to generate token')
+  }
+}
 
 export async function GET(req: NextRequest) {
   if (req.method !== 'GET') {
     return NextResponse.json({ message: 'Method not allowed' })
   }
 
-  const token = await generateBearerToken()
-
-  return NextResponse.json(token, {
-    headers: { 'Content-Type': 'application/json' },
-  })
+  try {
+    const token = await generateBearerToken()
+    return NextResponse.json(token, {
+      headers: { 'Content-Type': 'application/json' },
+    })
+  } catch {
+    return NextResponse.json({ error: 'failed' }, { status: 500 })
+  }
 }
